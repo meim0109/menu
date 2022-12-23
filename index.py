ac29479ddf20527e442fc3aad81e6bfa337283e4
meim0109 <s1101727@gm.pu.edu.tw>
2022-12-23 17:21:26 +0800

    make it better

--- index.py
+++ index.py
@@ -27,29 +27,29 @@ def account():
     else:
         return render_template("account.html")
 
-# @app.route("/search", methods=["GET", "POST"])
-# def search():
-#     if request.method == "POST":
-#         cond = request.form["time"]
-#         result = "請輸入您減肥菜單需要的時段：" + cond
+@app.route("/search", methods=["GET", "POST"])
+def search():
+     if request.method == "POST":
+         cond = request.form["time"]
+         result = "請輸入您減肥菜單需要的時段：" + cond
 
-#         db = firestore.client()
-#         collection_ref = db.collection("減肥菜單")
-#         docs = collection_ref.get()
-#         result = ""
-#         for doc in docs:
-#             dict = doc.to_dict()
-#             if cond in dict["time"]:
-#                 result += "您實施減肥菜單的天數："+ dict["date"] +"您今天選擇的時段是:"+ dict["time"] + "主食:"+ dict["Staple Food"] 
-#                 result += "副餐:" + dict["Nonstaple Food"]+ "飲品:" + dict["beverage"] + "水果or點心:"+ dict["fruit"]+ "<br>"
+         db = firestore.client()
+         collection_ref = db.collection("減肥菜單")
+         docs = collection_ref.get()
+         result = ""
+         for doc in docs:
+             dict = doc.to_dict()
+             if cond in dict["time"]:
+                 result += "您實施減肥菜單的天數："+ dict["date"] +"您今天選擇的時段是:"+ dict["time"] + "主食:"+ dict["Staple Food"] 
+                 result += "副餐:" + dict["Nonstaple Food"]+ "飲品:" + dict["beverage"] + "水果or點心:"+ dict["fruit"]+ "<br>"
             
-#         if result == "":
-#             result = "抱歉，查無相關條件的瘦身菜單"
+         if result == "":
+             result = "抱歉，查無相關條件的瘦身菜單"
 
-#         return result
+         return result
            
-#     else:
-#         return render_template("search.html")
+     else:
+         return render_template("search.html")
 
 @app.route("/webhook", methods=["POST"])
 def webhook():
