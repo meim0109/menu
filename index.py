a4de84abd6279ef23872a873cb3456926482aefb
meim0109 <s1101727@gm.pu.edu.tw>
2022-12-23 15:37:10 +0800

    make it better

--- index.py
+++ index.py
@@ -62,9 +62,6 @@ def webhook():
         menu =  req.get("queryResult").get("parameters").get("menu")
 
         info = "您要查詢減肥菜單的時段：" + menu + "，相關資料：\n"
-
-        collection_ref = db.collection("減肥菜單")
-        docs = collection_ref.get()
         result=""
         for doc in docs:
             dict = doc.to_dict()
